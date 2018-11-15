7925a09f6def34c574e55f420b82785be07bf2b1
rumsky <zhaohongyuu@163.com>
2018-11-15 17:40:18 +0800

    s

--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -37,9 +37,9 @@ class HomeStore {
   blockRewardChartData_BSV_usd;
 
   @observable
-  bchSecialCodeList;
+  bchSpecialCodeList;
   @observable
-  bsvSecialCodeList;
+  bsvSpecialCodeList;
 
   constructor() {
     this.loading = false;
@@ -51,34 +51,14 @@ class HomeStore {
     this.transactionChartData_BCH = [];
     this.transactionChartData_BSV = [];
     this.blockRewardXAxisChartData = [];
-    this.blockRewardChartData_BCH_rmb = [];
-    this.blockRewardChartData_BCH_usd = [];
-    this.blockRewardChartData_BSV_rmb = [];
-    this.blockRewardChartData_BSV_usd = [];
-    this.bchSecialCodeList = [
-      {
-        tx_hash:
-          '0xfd4389434334340xfd4389434334340xfd4389434334340xfd438943433434',
-        opcode_count: 3
-      },
-      {
-        tx_hash:
-          '0xfd4389434334340xfd4389434334340xfd4389434334340xfd438943433434',
-        opcode_count: 5
-      }
-    ];
-    this.bsvSecialCodeList = [
-      {
-        tx_hash:
-          '0xfd4389434334340xfd4389434334340xfd4389434334340xfd438943433434',
-        opcode_count: 3
-      },
-      {
-        tx_hash:
-          '0xfd4389434334340xfd4389434334340xfd4389434334340xfd438943433434',
-        opcode_count: 5
-      }
-    ];
+    //this.blockRewardChartData_BCH_rmb = [];
+    //this.blockRewardChartData_BCH_usd = [];
+    //this.blockRewardChartData_BSV_rmb = [];
+    //this.blockRewardChartData_BSV_usd = [];
+    this.blockRewardChartData_BCH = [];
+    this.blockRewardChartData_BSV = [];
+    this.bchSpecialCodeList = [];
+    this.bsvSpecialCodeList = [];
   }
 
   @action
@@ -168,7 +148,7 @@ class HomeStore {
     const res = await ajax.get(`/bch/code/list`);
     if (res && res.data) {
       runInAction(() => {
-        this.bchSecialCodeList = res.data.list;
+        this.bchSpecialCodeList = res.data.list;
       });
     }
   };
